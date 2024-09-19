4231b7997d0302622f6792d82a4f7e7243788fb7
epicontentyt <epicontent.yt@gmail.com>
2024-09-19 20:12:42 -0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to EpicTube!</h1>
+            <p>Your video platform awaits.</p>
+        </div>
+    );
+};
+
+export default App;
